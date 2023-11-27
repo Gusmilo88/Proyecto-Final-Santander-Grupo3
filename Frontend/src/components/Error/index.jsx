a837f56b15ff92a606c08990d8d6eafdacdf55b9
Gus <gus_eze1988@hotmail.com>
2023-11-27 16:06:34 -0300

    header 404 footer y todas las images terminado

--- Frontend/src/components/Error/index.jsx
+++ Frontend/src/components/Error/index.jsx
@@ -1,19 +1,27 @@
+import { Link } from 'react-router-dom'
 import './style.css';
 
 function Error() {
     return (
         <>
-
-            <div className="circles">
-                <p>404<br />
-                    <small>PAGE NOT FOUND</small>
-                </p>
-                <span className="circle big" />
-                <span className="circle med" />
-                <span className="circle small" />
-            </div>
-
-        </>
+    <div className='container404'>
+    <div className='containerIMG'>
+      <div className='containerAstronaut'>
+        <img src="/src/assets/images/astronaut.png" alt="Imagen de un astronauta" />
+      </div>
+      <div className='containerWorld'>
+        <Link to={"/"}><img src="/src/assets/images/world.png" alt="Imagen del planeta Tierra"/></Link>
+      </div>
+      <div className='containerDog'>
+        <img src="/src/assets/images/dog.png" alt="Imagen de un perro astronauta" />
+      </div>
+    </div>
+    <div className='containerText'>
+      <h2 className='h2404'>404 Not found</h2>
+      <p className='p404'>Tocá el planeta para volver</p>
+    </div>
+    </div>
+    </>
     )
 }
 
--- Frontend/src/components/Header/index.jsx
+++ Frontend/src/components/Header/index.jsx
@@ -1,58 +1,140 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './style.css';
+import { Link } from "react-router-dom";
+import "./style.css";
 
-function Header() {
+const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src="../src/assets/img/AMAZING.png" alt="Logo" width={125} />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <NavLink to="/" className="btn btn-sm nav-link me-2" activeClassName="active" id="home-btn">
-                <strong className="bold-text">HOME</strong>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/upcoming" className="btn btn-sm nav-link me-2" activeClassName="active" id="upcoming-events-btn">
-                <strong className="bold-text">UPCOMING EVENTS</strong>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/past" className="btn btn-sm nav-link me-2" activeClassName="active" id="past-events-btn">
-                <strong className="bold-text">PAST EVENTS</strong>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/contact" className="btn btn-sm nav-link me-2" activeClassName="active" id="contact-btn">
-                <strong className="bold-text">CONTACT</strong>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/stats" className="btn btn-sm nav-link me-2" activeClassName="active" id="stats-btn">
-                <strong className="bold-text">STATS</strong>
-              </NavLink>
-            </li>
-
-          </ul>
+    <div>
+      <nav className="dark:bg-zinc-900 border-gray-200 px-4 lg:px-6 py-2.5 ">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to={"/"}>
+            <img
+              id="imgLogo"
+              className="h-16 w-auto"
+              src="/src/assets/images/logo.png"
+              alt="Logo Amazing Events"
+            />
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <button
+              id="hamburgerButton"
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2.5 ml-1 text-sm text-white rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link
+                  id="iconsNavMobile"
+                  to={"/"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-house"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/upcomingEvents"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-calendar"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  Upcoming Events
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/pastEvents"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-calendar-check"
+                    style={{ color: "#fcfcfc" }}
+                  ></i>
+                  Past Events
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/contact"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-envelope"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/stats"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-chart-line"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  Stats
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/login"}
+                  className="text-white block rounded-md px-3 py-2 text-base font-medium"
+                >
+                  <i
+                    className="fa-solid fa-user"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      
+    </div>
   );
-}
+};
 
 export default Header;
