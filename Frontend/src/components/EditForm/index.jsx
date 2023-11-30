6efb9ae575637f90ad0728862e461f3265469ebd
Andrea Luque <andreavaninaluque@icloud.com>
2023-11-30 02:22:40 -0300

    commit andrea

--- Frontend/src/components/EditForm/index.jsx
+++ Frontend/src/components/EditForm/index.jsx
@@ -87,33 +87,7 @@ const EditForm = ({ onCancel }) => {
               <h2 className="text-white py-3">
                 <strong>CHANGE PASSWORD</strong>
               </h2>
-              <label>
-              Old password:
-              <input
-                type="password"
-                name="contrasenaActual"
-                value={formData.contrasenaActual}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              New password:
-              <input
-                type="password"
-                name="nuevaContrasena"
-                value={formData.nuevaContrasena}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Confirm new password :
-              <input
-                type="password"
-                name="nuevaContrasena"
-                value={formData.nuevaContrasena}
-                onChange={handleInputChange}
-              />
-            </label>
+              {/* Resto de los campos para cambiar contraseña */}
             </div>
           </div>
 
@@ -122,108 +96,7 @@ const EditForm = ({ onCancel }) => {
               <h2 className="text-white py-3">
                 <strong>PAYMENT INFORMATION</strong>
               </h2>
-              <label>
-              Pay with:
-              <button>Credit or debit card</button>
-              <button>PayPal account</button>
-            </label>
-            <label>
-              Card Number:
-              <input
-                type="text"
-                name="numeroTarjeta"
-                value={formData.numeroTarjeta}
-                onChange={handleInputChange}
-              />
-            </label>
-            <div class="form-label-input-pair">
-              <label
-                class="form-label required"
-                id="form-label-creditCardExpirationMonth"
-                for="input-creditCardExpirationMonth"
-              >
-                Expiration Date (MM/YY)
-              </label>
-              <label
-                class="form-label form-label-month"
-                for="input-creditCardExpirationMonth"
-              >
-                
-              </label>Month /
-              <select
-                encrypt="true"
-                class="select-input "
-                id="input-creditCardExpirationMonth"
-                name="field_creditCardExpirationMonth"
-                aria-required="true"
-                aria-label="Expiration Month"
-                aria-describedby="error-creditCardExpirationMonth"
-              >
-                <option value="">- Select One -</option>
-                <option value="01">01</option>
-                <option value="02">02</option>
-                <option value="03">03</option>
-                <option value="04">04</option>
-                <option value="05">05</option>
-                <option value="06">06</option>
-                <option value="07">07</option>
-                <option value="08">08</option>
-                <option value="09">09</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-              </select>
-            </div>
-            <label
-              class="form-label form-label-year"
-              for="input-creditCardExpirationYear"
-            >
-              
-            </label>Year
-            <select
-              encrypt="true"
-              class="select-input "
-              id="input-creditCardExpirationYear"
-              name="field_creditCardExpirationYear"
-              aria-required="true"
-              aria-label="Expiration Year"
-              aria-describedby="error-creditCardExpirationMonth"
-            >
-              <option value="">- Select One -</option>
-              <option value="2023">2023</option>
-              <option value="2024">2024</option>
-              <option value="2025">2025</option>
-              <option value="2026">2026</option>
-              <option value="2027">2027</option>
-              <option value="2028">2028</option>
-              <option value="2029">2029</option>
-              <option value="2030">2030</option>
-              <option value="2031">2031</option>
-              <option value="2032">2032</option>
-              <option value="2033">2033</option>
-              <option value="2034">2034</option>
-              <option value="2035">2035</option>
-              <option value="2036">2036</option>
-              <option value="2037">2037</option>
-              <option value="2038">2038</option>
-              <option value="2039">2039</option>
-              <option value="2040">2040</option>
-              <option value="2041">2041</option>
-              <option value="2042">2042</option>
-              <option value="2043">2043</option>
-              <option value="2043">2044</option>
-              <input
-                type="text"
-                id="input-cardSecurityCode"
-                encrypt="true"
-                value=""
-                class="text-input-cvv"
-                name="field_cardSecurityCode"
-                maxlength="4"
-                aria-describedby="error-cardSecurityCode">
-                </input>
-            </select>
-            &nbsp;
+              {/* Resto de los campos de información de pago */}
             </div>
           </div>
 
