9fc419ca1ce7191fbb65f7c458acaf7e552d457d
Andrea Luque <andreavaninaluque@icloud.com>
2023-11-30 11:58:33 -0300

    commit

--- Frontend/src/components/EditForm/index.jsx
+++ Frontend/src/components/EditForm/index.jsx
@@ -1,5 +1,5 @@
-import './style.css';
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./style.css";
 
 const EditForm = ({ onCancel }) => {
     const [formData, setFormData] = useState({
@@ -36,19 +36,19 @@ const EditForm = ({ onCancel }) => {
                 <form onSubmit={handleSubmit}>
                     <div className="contact-info">
                     <h2 className="text-white py-3"><strong>INFORMACION DE CONTACTO</strong></h2>
-                        <label>
+                        <label className="editForm">
                             Nombre:
                             <input type="text" name="nombre" value={formData.nombre} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Apellido:
                             <input type="text" name="apellido" value={formData.apellido} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Teléfono:
                             <input type="text" name="telefono" value={formData.telefono} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Domicilio:
                             <input type="text" name="direccion" value={formData.direccion} onChange={handleInputChange} />
                         </label>
@@ -56,11 +56,11 @@ const EditForm = ({ onCancel }) => {
 
                     <div className="change-password">
                     <h2 className="text-white py-3"><strong>CAMBIAR CONTRASEÑA</strong></h2>
-                        <label>
+                        <label className="editForm">
                             Contraseña Actual:
                             <input type="password" name="contrasenaActual" value={formData.contrasenaActual} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Nueva Contraseña:
                             <input type="password" name="nuevaContrasena" value={formData.nuevaContrasena} onChange={handleInputChange} />
                         </label>
@@ -69,19 +69,19 @@ const EditForm = ({ onCancel }) => {
 
                     <div className="payment-method">
                         <h2 className="text-white py-3"><strong>METODO DE PAGO</strong></h2>
-                        <label>
+                        <label className="editForm">
                             Número de Tarjeta:
                             <input type="text" name="numeroTarjeta" value={formData.numeroTarjeta} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             CVC:
                             <input type="text" name="cvc" value={formData.cvc} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Fecha de Expiración:
                             <input type="text" name="fechaExpiracion" value={formData.fechaExpiracion} onChange={handleInputChange} />
                         </label>
-                        <label>
+                        <label className="editForm">
                             Tipo de Tarjeta:
                             <select name="tipoTarjeta" value={formData.tipoTarjeta} onChange={handleInputChange}>
                                 <option value="visa">Visa</option>
@@ -99,4 +99,3 @@ const EditForm = ({ onCancel }) => {
 };
 
 export default EditForm;
-
